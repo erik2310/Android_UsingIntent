2597cbbfdbda0e529ef6eb66ace8618e84686567
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-05-04 01:23:17 +0200

    Initial commit

---
+++ app/src/main/java/erik2310/usingintent/MainActivity.java
@@ -0,0 +1,19 @@
+package erik2310.usingintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent("erik2310.usingintent.SecondActivity"));
+    }
+}
